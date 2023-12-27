1a3222f9b5f07c27bf529e99907e396c86edfb6c
Maksym Korchak <Mkorchak1992@gmail.com>
2023-12-27 18:10:33 +0200

    Fix Form validation func

--- components/Product/Form/index.js
+++ components/Product/Form/index.js
@@ -12,34 +12,48 @@ type ProductFormProps  = {
     initialValues?: object | null;
 };
 
+type ProductValues = {
+    name?: string;
+    category?: string;
+    description?: string;
+    price?: string;
+  };
+  
+  const validateProduct = (values: ProductValues) => {
+    const errors = {};
+  
+    if (!values.name) {
+      errors.name = "Field is required";
+    } else if (values.name.length < 3) {
+      errors.name = "Should be at least 3 characters";
+    }
+  
+    if (!values.category) {
+      errors.category = "Field is required";
+    } else if (values.category.length < 3) {
+      errors.category = "Should be at least 3 characters";
+    }
+  
+    if (!values.description) {
+      errors.description = "Field is required";
+    } else if (values.description.length < 20) {
+      errors.description = "Should be at least 20 characters";
+    }
+  
+    if (!/^\d+$/.test(values.price)) {
+      errors.price = 'Should be a numeric value';
+    }
+  
+    return errors;
+  };
+
   function ProductForm ({ handleSubmit, initialValues }: ProductFormProps) {
 
     return (
         <Form 
             onSubmit={handleSubmit}
             initialValues={initialValues} 
-            validate={(values) => {
-                const errors = {};
-                    switch (true) {
-                        case !values.name:
-                            errors.name = "Field is required";
-                        case values.name.length < 3:
-                            errors.name = "Should be at least 3 characters";
-                        case !values.category:
-                            errors.category = "Field is required";
-                        case values.category.length < 3:
-                            errors.category = "Should be at least 3 characters";
-                        case !values.description:
-                            errors.description = "Field is required";
-                        case values.description.length < 20:
-                            errors.description = "Should be at least 20 characters";
-                        case !/^\d+$/.test(values.price):
-                            errors.price = 'Should be a numeric value';
-                        default:
-                            break;
-                    }
-                return errors;
-            }}
+            validate={validateProduct}
             render={({ handleSubmit, valid }) => (
             <>
                 <Field name="name">
